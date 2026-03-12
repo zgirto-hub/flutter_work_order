467661ec4a56c6f3a7a55572929df5bc4af03ba4
zgirto-hub <zgirto@gmail.com>
2026-03-13 00:29:28 +0300

    backend update version

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,7 @@ from PyPDF2 import PdfReader
 from docx import Document
 
 import pytesseract
+import json
 from pdf2image import convert_from_path
 
 
@@ -23,10 +24,24 @@ SUPABASE_URL = "https://rydrqsjofoulwdtwfbgv.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5ZHJxc2pvZm91bHdkdHdmYmd2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjE0MTg5MiwiZXhwIjoyMDg3NzE3ODkyfQ.HvebR7mHIz2Dp4HRiLf6nVrzbqgeIX5XLc3NuVexwII"  # VERY IMPORTANT
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# --------------------
+# Check for Update 
+# --------------------
+import json
+
+@app.get("/api/version")
+def get_version():
+    with open("version.json") as f:
+        return json.load(f)
+
+
 # --------------------
 # FastAPI Setup
 # --------------------
 app = FastAPI()
+
+
 
 UPLOAD_DIR = "uploaded_files"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
